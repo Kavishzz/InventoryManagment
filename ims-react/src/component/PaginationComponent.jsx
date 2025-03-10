7ef57a8618c724d43da0a4af935e3b0116a17c8a
Kavishzz <kavishmorey123@gmail.com>
2025-03-10 22:41:39 +0530

    [KAVISH] : created component and service files.

---
+++ ims-react/src/component/PaginationComponent.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const PaginationComponent = ({crrentPage, totalPages, onPageChange}) => {
+    const pageNumbers = Array.from({length: totalPages}, (_, i) => i+1);
+
+    return(
+        <div className="pagination-container">
+            <button className="pagination-button"
+            disabled={crrentPage === 1}
+            onClick={() => onPageChange(crrentPage - 1)}>
+                &laquo; Prev
+            </button>
+
+            {pageNumbers.map((number) => (
+                <button key={number}
+                className={`pagination-button ${crrentPage == number ? "active": ""} ` }
+                onClick={() => onPageChange(number)}>
+                {number}
+                </button>
+            ))}
+
+            <button className="pagination-container"
+            disabled={crrentPage === totalPages}
+            onClick={() => onPageChange(crrentPage + 1)}>
+                Next &raquo;
+            </button>
+        </div>
+    )
+}
